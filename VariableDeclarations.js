ab82cee193661313e9cc3cefbf972ec9dfc2119b
Pawan KC <pawan.kc@cotiviti.com>
2020-11-25 13:01:35 +0545

    JSLearning - Variable

---
+++ VariableDeclarations.js
@@ -0,0 +1,24 @@
+//Datatypes
+x = 89;
+x = "pawan";
+x = true;
+
+//Primary
+var a = "Pawan";
+var b = true;
+var c = 123345;
+var d = 2n ** 200n;
+var e;
+var f = null;
+
+//Structural
+var g = [2, 3, 4, 5];
+var h = {name: "Pawan" , address:"Imadole" , isMale: true}
+
+
+
+console.log(x);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(h);
